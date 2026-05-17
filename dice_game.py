d6b69474b5de62a788f2f1babec09216152c4a4a
arammn <aramm6013@gmail.com>
2026-05-17 01:20:34 -0700

    Update dice_game.py

--- dice_game.py
+++ dice_game.py
@@ -1,4 +1,4 @@
-"""Dice Game mini‑game – reacts to Telegram dice."""
+"""Dice Game – reacts to Telegram dice."""
 import logging, html
 from config import Config
 from database import Database
@@ -6,51 +6,28 @@ from database import Database
 logger = logging.getLogger(__name__)
 
 class DiceGameManager:
-    def __init__(self):
-        self.db = Database()
+    def __init__(self): self.db = Database()
 
     async def process_message(self, update, context):
         msg = update.effective_message
-        if not msg or not msg.dice:
-            return
+        if not msg or not msg.dice: return
         chat_id = update.effective_chat.id
         user = update.effective_user
 
         game = await self.db.get_active_dice_game(chat_id)
-        if not game or user.is_bot:
-            return
-        if msg.dice.emoji != game['dice_emoji']:
-            return
-        if msg.dice.value != game['winning_value']:
-            return
+        if not game or user.is_bot: return
+        if await self.db.is_ignored(chat_id, user.id): return
+        if msg.dice.emoji != game['dice_emoji']: return
+        if msg.dice.value != game['winning_value']: return
 
         prize = html.escape(game['prize'])
-        if user.username:
-            winner = f"@{user.username}"
-        else:
-            winner = html.escape(user.full_name)
-        win_msg = await msg.reply_text(
-            f"🎲 <b>Победа в игре!</b>\n"
-            f"<b>{winner}</b> выбил {game['winning_value']} и выиграл: {prize}!",
-            parse_mode="HTML"
-        )
-        try:
-            await context.bot.pin_chat_message(chat_id, win_msg.message_id, disable_notification=False)
-        except Exception as e:
-            logger.error(f"Could not pin dice win message: {e}")
-
+        if user.username: winner = f"@{user.username}"
+        else: winner = html.escape(user.full_name)
+        win_msg = await msg.reply_text(f"🎲 <b>Победа в игре!</b>\n<b>{winner}</b> выбил {game['winning_value']} и выиграл: {prize}!", parse_mode="HTML")
+        try: await context.bot.pin_chat_message(chat_id, win_msg.message_id, disable_notification=False)
+        except: pass
         for admin_id in Config.ADMIN_IDS:
-            dm_text = (
-                f"🎲 <b>Dice Game Win!</b>\n"
-                f"Группа: <code>{chat_id}</code>\n"
-                f"Победитель: <b>{winner}</b>\n"
-                f"Приз: {prize}\n"
-                f"ID: <code>{user.id}</code>\n"
-                f"Эмодзи: {msg.dice.emoji}, значение: {msg.dice.value}"
-            )
-            if user.username:
-                dm_text += f"\nЮзернейм: @{user.username}"
-            try:
-                await context.bot.send_message(admin_id, dm_text, parse_mode="HTML")
-            except Exception as e:
-                logger.error(f"Failed to notify admin {admin_id}: {e}")
+            dm_text = f"🎲 <b>Dice Win!</b>\nГруппа: <code>{chat_id}</code>\nПобедитель: <b>{winner}</b>\nПриз: {prize}\nID: <code>{user.id}</code>\nЭмодзи: {msg.dice.emoji}, значение: {msg.dice.value}"
+            if user.username: dm_text += f"\nЮзернейм: @{user.username}"
+            try: await context.bot.send_message(admin_id, dm_text, parse_mode="HTML")
+            except: pass
